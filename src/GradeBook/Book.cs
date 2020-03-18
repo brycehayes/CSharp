b2f0ee125b989df1d27b8b080f829389b73e745a
brycehayes-govscot <59962963+brycehayes-govscot@users.noreply.github.com>
2020-03-18 14:52:35 +0000

    Changes to Book class for encapsulation

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -7,11 +8,35 @@ namespace GradeBook
 
         private List<double> grades;
 
+        double lowGrade = double.MaxValue;
+        double highGrade = double.MinValue;
+        double result = 0.0;
+
         public Book() {
             grades = new List<double>();
         }
         public void AddGrade(double grade) {
             grades.Add(grade);
+        }
+
+        public void showStatistics() {
+
+            foreach(double grade in grades) {
+            
+                if(grade > highGrade) {
+                    highGrade = grade;
+                }
+                if(grade < lowGrade) {
+                    lowGrade = grade;
+                }
+
+                result = result + grade;
+
+            }
+            System.Console.WriteLine("Test");
+             System.Console.WriteLine($"The average Result is {result/grades.Count}");
+             System.Console.WriteLine($"The highest grade is: {highGrade}");
+             System.Console.WriteLine($"The lowest grade is: {lowGrade}");
         }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,22 +10,32 @@ namespace GradeBook
 
            var book = new Book();
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.showStatistics();
             
+        //    var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};;
+        //    grades.Add(56.1);
+           
 
+        //    var result = 0.0;
+        //    var highGrade = double.MinValue;
+        //    var lowGrade = double.MaxValue;
+        //    foreach(var number in grades) {
+        //        if(number > highGrade) {
+        //            highGrade = number;
+        //        }
+        //        if (number < lowGrade) {
+        //            lowGrade = number;
+        //        } 
 
+        //        result = result + number;
+        //    }
 
-           var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};;
-           grades.Add(56.1);
-           grades.Add(46.1);
-
-           var result = 0.0;
-           foreach(var number in grades) {
-               result = result + number;
-           }
-
-            System.Console.WriteLine($"Average grade is:  {result/grades.Count:N3}");
-            System.Console.WriteLine("Result of arrays is: " + result);
-
+        //     System.Console.WriteLine($"Average grade is:  {result/grades.Count:N3}");
+        //     System.Console.WriteLine("Result of arrays is: " + result);
+        //     System.Console.WriteLine("Highest Grade: " + highGrade);
+        //     System.Console.WriteLine("Lowest Grade: " + lowGrade);
             
         }
     }
